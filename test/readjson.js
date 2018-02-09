6f83d244e4b018ec637f1b3b9c06a41b0397db98
coderaiser <coderaiser@cloudcmd.io>
2018-02-09 13:42:58 +0200

    chore(readjson) let -> const

--- test/readjson.js
+++ test/readjson.js
@@ -1,12 +1,12 @@
 'use strict';
 
-let path        = require('path');
-let test        = require('tape');
-let readjson   = require('..');
+const path = require('path');
+const test = require('tape');
+const readjson = require('..');
 
 const NAME = path.join(__dirname, '..', 'package.json');
 
-test('readjson: should read json data from file', t => {
+test('readjson: should read json data from file', (t) => {
     readjson(NAME, (error, json)  => {
         t.notOk(error, 'no read error');
         t.equal(typeof json, 'object', 'json should be object');
@@ -14,31 +14,31 @@ test('readjson: should read json data from file', t => {
     });
 });
 
-test('readjson.sync.try: should read json data from file', t => {
+test('readjson.sync.try: should read json data from file', (t) => {
     let json = readjson.sync.try(NAME);
     
     t.equal(typeof json, 'object', 'json should be object');
     t.end();
 });
 
-test('readjson: no args', t => {
+test('readjson: no args', (t) => {
     t.throws(readjson, /name should be string!/, 'NAME check');
     t.end();
 });
 
-test('readjson: no callback', t => {
+test('readjson: no callback', (t) => {
     let fn = () => readjson('hello', [1,2,3]);
     
     t.throws(fn, /callback should be function!/, 'callback check');
     t.end();
 });
 
-test('readjson.sync: no args', t => {
+test('readjson.sync: no args', (t) => {
     t.throws(readjson.sync, /name should be string!/, 'NAME check');
     t.end();
 });
 
-test('readjson.sync.try: no args', t => {
+test('readjson.sync.try: no args', (t) => {
     t.throws(readjson.sync.try, /name should be string!/, 'NAME check');
     t.end();
 });
